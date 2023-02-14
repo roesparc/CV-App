42b581222d41031016cc29493f2f42932f90b551
Rodrigo Espinosa <rodesar95@hotmail.com>
2023-02-14 16:55:17 -0600

    Add a main element with the CV component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
+import Cv from "./components/Cv";
 
 class App extends React.Component {
   render() {
-    return <div className="App"></div>;
+    return (
+      <main>
+        <Cv />
+      </main>
+    );
   }
 }
 
