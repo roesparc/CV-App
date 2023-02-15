551a8df4ecd99ea0e115f5cf7627fb86bf766c49
Rodrigo Espinosa <rodesar95@hotmail.com>
2023-02-15 13:11:07 -0600

    Add template content file

---
+++ src/assets/templateContent.js
@@ -0,0 +1,13 @@
+import examplePhoto from "./example-photo.jpg";
+
+export const samplePersonalInfo = {
+  name: "John Doe",
+  title: "Software Developer",
+  mail: "john_doe@mail.com",
+  phone: "012 345 6789",
+  location: "Mexico City",
+  photoUrl: examplePhoto,
+};
+
+export const introductionContent =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
