d52fbe3f20c020f5cb6e103ea82e815630ade582
Rodrigo Espinosa <rodesar95@hotmail.com>
2023-02-16 19:05:26 -0600

    Add header and footer sections to the app

--- src/App.js
+++ src/App.js
@@ -4,9 +4,24 @@ import Cv from "./components/Cv";
 class App extends React.Component {
   render() {
     return (
-      <main>
-        <Cv />
-      </main>
+      <div className="App">
+        <header>
+          <h1>CV App</h1>
+        </header>
+
+        <main>
+          <Cv />
+        </main>
+
+        <footer>
+          <p>
+            By <a href="https://github.com/roesparc/">roesparc</a>
+          </p>
+          <a href="https://github.com/roesparc/">
+            <i class="fa-brands fa-github"></i>
+          </a>
+        </footer>
+      </div>
     );
   }
 }
