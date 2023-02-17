fe2c22544fd83152f764eaee25417fee161dd4a4
Rodrigo Espinosa <rodesar95@hotmail.com>
2023-02-16 19:08:18 -0600

    Update import paths for the CV components

--- src/components/Cv.js
+++ src/components/Cv.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "../styles/Cv.css";
-import PersonalInfo from "./PersonalInfo";
-import Introduction from "./Introduction";
-import WorkExperience from "./WorkExperience";
-import Education from "./Education";
+import PersonalInfo from "./PersonalInfo/PersonalInfo";
+import Introduction from "./Introduction/Introduction";
+import WorkExperience from "./WorkExperience/WorkExperience";
+import Education from "./Education/Education";
 
 class Cv extends React.Component {
   render() {
