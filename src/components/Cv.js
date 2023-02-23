3ed1f5931a04550f58f7aab6e09f9214b4e7cde3
Rodrigo Espinosa <rodesar95@hotmail.com>
2023-02-23 12:42:03 -0600

    Add print functionality

--- src/components/Cv.js
+++ src/components/Cv.js
@@ -1,21 +1,31 @@
-import React from "react";
+import { useRef } from "react";
+import { useReactToPrint } from "react-to-print";
+
 import "../styles/Cv.css";
+
 import PersonalInfo from "./PersonalInfo/PersonalInfo";
 import Introduction from "./Introduction/Introduction";
 import WorkExperience from "./WorkExperience/WorkExperience";
 import Education from "./Education/Education";
 
-class Cv extends React.Component {
-  render() {
-    return (
-      <div className="cv-create">
-        <PersonalInfo />
-        <Introduction />
-        <WorkExperience />
-        <Education />
-      </div>
-    );
-  }
-}
+const Cv = () => {
+  const cvRef = useRef();
+  const handlePrint = useReactToPrint({
+    content: () => cvRef.current,
+  });
+
+  return (
+    <div className="cv-create" ref={cvRef}>
+      <PersonalInfo />
+      <Introduction />
+      <WorkExperience />
+      <Education />
+
+      <button className="print-button" onClick={handlePrint}>
+        <i className="fa-solid fa-file-pdf"></i>
+      </button>
+    </div>
+  );
+};
 
 export default Cv;
